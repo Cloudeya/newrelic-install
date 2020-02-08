37b2fe64fa6fe6833565891eceafb95a9ca502d2
Rajveer Beerda <rajveerbeerda98@gmail.com>
2020-02-09 00:35:06 +0530

    Added form key for therapists form

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 subscribe_form_key = "ee536008-234e-4a24-9d3d-d53b1d5caaa7"
 workshop_form_key = "4d60cf63-63bc-4a9d-ad80-df27699fa21a"
 partnership_form_key = "9abcf80b-13ad-49df-a4b5-b2dab6f897f0"
+therapist_form_key = "f0e1a3da-319d-47f7-b6d7-0b151da77558"
 hubspot_key = "6894081"
