be38f5c6b50928f561d39e1c30ce0ded038cdfee
Rajveer Beerda <rajveerbeerda98@gmail.com>
2020-02-09 00:36:07 +0530

    added function for hubspot therapists form

--- hubspot.py
+++ hubspot.py
@@ -39,6 +39,25 @@ def workshop_forms(email_id, first_name, last_name, company_name, message):
 	else:
 		return 0
 
+
+def therapist_forms(email_id, first_name, last_name, company_name, message):
+	endpoint = hubspot_url + config.hubspot_key + '/' + config.therapist_form_key + '?&'
+
+	# Build your request body
+	data = urllib.parse.urlencode({
+		'email': email_id,
+		'firstname': first_name,
+		'lastname': last_name,
+		'company': company_name,
+		'details': message
+	})
+	r = requests.post(url=endpoint, data=data, headers=headers)
+	response = str(r)[-5:-2]
+	if response=="204":
+		return 1
+	else:
+		return 0
+
 def partnership_forms(email_id, first_name, last_name="", company_name="", message = ""):
 	endpoint = hubspot_url + config.hubspot_key + '/' + config.partnership_form_key + '?&'
 
